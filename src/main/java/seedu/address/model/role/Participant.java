1d19035c7ac4c86f264d8f16febadf76e119b0bd
weizhang05 <chanweizhang05@hotmail.com>
2019-03-02 16:23:03 +0800

    modified Participant class. extends on Person class.

--- src/main/java/seedu/address/model/role/Participant.java
+++ src/main/java/seedu/address/model/role/Participant.java
@@ -1,7 +1,34 @@
 package seedu.address.model.role;
 
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Phone;
+import seedu.address.model.privilege.Privilege;
+import seedu.address.model.tag.Tag;
+
+import java.util.Set;
+
 /**
  * Participant for FOP.
  */
-public class Participant {
+public class Participant extends Person {
+
+    private Privilege privilege;
+
+    /**
+     * Every field must be present and not null.
+     *
+     * @param name
+     * @param phone
+     * @param email
+     * @param address
+     * @param tags
+     */
+    public Participant(Name name, Phone phone, Email email, Address address, Set<Tag> tags) {
+        super(name, phone, email, address, tags);
+        // By default everybody is a participant
+        this.privilege = Privilege.PARTICIPANTS;
+    }
 }
