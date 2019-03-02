e4baeab46fc2017b7cd2352081092c3694528b80
weizhang05 <chanweizhang05@hotmail.com>
2019-03-02 22:10:56 +0800

    created skeleton for randomize command

---
+++ src/main/java/seedu/address/logic/commands/RandomizeCommand.java
@@ -0,0 +1,19 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Randomly assigns all participants to all available groups.
+ * Evenly distributes the participants (number differs from either 1 or 2).
+ */
+public class RandomizeCommand extends Command {
+
+    public static final String COMMAND_WORD = "randomize";
+
+    @Override
+    public CommandResult execute(Model model, CommandHistory history) throws CommandException {
+        return null;
+    }
+}
