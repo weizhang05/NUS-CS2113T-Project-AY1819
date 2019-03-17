1d4b8032ffd07fecbb675a30f6ff5cb258aed988
ssunil3232 <soniasunil47@gmail.com>
2019-03-17 17:55:44 +0800

    Listing of Freshmen

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -19,6 +19,7 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListFreshmenCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.UndoCommand;
@@ -80,6 +81,9 @@ public class AddressBookParser {
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
+
+        case ListFreshmenCommand.COMMAND_WORD:
+            return new ListFreshmenCommand();
 
         case HistoryCommand.COMMAND_WORD:
             return new HistoryCommand();
