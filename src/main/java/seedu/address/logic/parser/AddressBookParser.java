1bcd44719ad8b95900d126902e009ee9b2f607b0
ssunil3232 <soniasunil47@gmail.com>
2019-03-17 21:09:26 +0800

    Listing out the Freshmen. Temporary ArrayList to store.

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -19,6 +19,7 @@ import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.ListParticipantCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
 import seedu.address.logic.commands.UndoCommand;
@@ -80,6 +81,9 @@ public class AddressBookParser {
 
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
+
+        case ListParticipantCommand.COMMAND_WORD:
+            return new ListParticipantCommand();
 
         case HistoryCommand.COMMAND_WORD:
             return new HistoryCommand();
