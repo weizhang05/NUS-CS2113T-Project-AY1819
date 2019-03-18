e64944eabd18f593eb8db601680595a588eae792
ssunil3232 <soniasunil47@gmail.com>
2019-03-18 21:57:17 +0800

    Merge Update

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -7,7 +7,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import seedu.address.logic.commands.AddCommand;
-import seedu.address.logic.commands.AddFreshmanCommand;
 import seedu.address.logic.commands.AddGroupCommand;
 import seedu.address.logic.commands.AddHouseCommand;
 import seedu.address.logic.commands.AddParticipantCommand;
@@ -55,9 +54,6 @@ public class AddressBookParser {
 
         case AddCommand.COMMAND_WORD:
             return new AddCommandParser().parse(arguments);
-
-        case AddFreshmanCommand.COMMAND_WORD:
-            return new AddFreshmanCommandParser().parse(arguments);
 
         case AddGroupCommand.COMMAND_WORD:
             return new AddGroupCommandParser().parse(arguments);
