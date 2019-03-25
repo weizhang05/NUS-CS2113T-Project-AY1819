1a4cb395bf7b75991030752bd73d1993da6bc8f7
weizhang05 <chanweizhang05@hotmail.com>
2019-03-25 22:17:15 +0800

    clean up Model interface

--- src/main/java/seedu/address/model/Model.java
+++ src/main/java/seedu/address/model/Model.java
@@ -82,35 +82,18 @@ public interface Model {
     /** Returns an unmodifiable view of the filtered person list */
     ObservableList<Person> getFilteredPersonList();
 
-    /** Returns an unmodifiable view of the undo list */
-    ObservableList<String> getUndoList();
-
-    /** Returns an unmodifiable view of the redo list */
-    ObservableList<String> getRedoList();
-
-    /**
-     * Returns true if a House with the same name as {@code House} exists in the address book.
-     */
-    boolean hasHouse(String house);
-
-    /**
-     * Adds the given House into address book.
-     * {@code house} must not already exist in the address book.
-     */
-    void addHouse(String house);
-
-    /**
-     * Returns House object.
-     */
-    House getHouse(String house);
-
-    void addGroup(String groupName, String houseName);
-
     /**
      * Updates the filter of the filtered person list to filter by the given {@code predicate}.
      * @throws NullPointerException if {@code predicate} is null.
      */
     void updateFilteredPersonList(Predicate<Person> predicate);
+
+    // ================ Undo/Redo functions ======================
+    /** Returns an unmodifiable view of the undo list */
+    ObservableList<String> getUndoList();
+
+    /** Returns an unmodifiable view of the redo list */
+    ObservableList<String> getRedoList();
 
     /**
      * Returns true if the model has previous address book states to restore.
