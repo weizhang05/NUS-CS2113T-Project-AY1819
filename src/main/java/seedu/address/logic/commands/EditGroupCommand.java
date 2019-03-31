1453c2a7a18431195ab7c8abea47fe5a53af9de1
shanseet <e0202323@u.nus.edu>
2019-03-31 22:09:18 +0800

    fix editgroup to edit all people in old group to new group

--- src/main/java/seedu/address/logic/commands/EditGroupCommand.java
+++ src/main/java/seedu/address/logic/commands/EditGroupCommand.java
@@ -2,11 +2,15 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
+
 import javafx.collections.ObservableList;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.grouping.Group;
+import seedu.address.model.participant.Person;
 
 /**
  * Edits the name of a group.
@@ -47,30 +51,46 @@ public class EditGroupCommand extends Command {
         if (newGroupName.equals(oldGroupName)) {
             throw new CommandException(MESSAGE_REPEAT_GROUP);
         }
-        ObservableList<Group> groupList = model.getFilteredGroupList();
+
+        ReadOnlyAddressBook existingAddressBook = model.getAddressBook();
+        ObservableList<Group> groupList = existingAddressBook.getGroupList();
 
         Group oldGroup = new Group(oldGroupName);
         Group newGroup = new Group(newGroupName);
 
         boolean nonexistent = true;
+        boolean existentNew = false;
 
         for (Group toList : groupList) {
             if (toList.getGroupName().equals(oldGroupName)) {
                 oldGroup.setHouseName(toList.getHouseName());
                 newGroup.setHouseName(toList.getHouseName());
                 nonexistent = false;
-                break;
             }
             if (toList.getGroupName().equals(newGroupName)) {
-                throw new CommandException(MESSAGE_EXISTENT_NEW_GROUP);
+                existentNew = true;
+                break;
             }
         }
 
         if (nonexistent) {
             throw new CommandException(MESSAGE_NONEXISTENT_OLD_GROUP);
         }
+        if (existentNew) {
+            throw new CommandException(MESSAGE_EXISTENT_NEW_GROUP);
+        }
 
+        //updates persons with old group to new group
+        List<Person> personList = existingAddressBook.getPersonList();
+        for (Person person : personList) {
+            if (person.getGroup().equals(oldGroup)) {
+                Person editedPerson = new Person(person.getName(), person.getSex(), person.getBirthday(),
+                        person.getPhone(), person.getEmail(), person.getMajor(), newGroup, person.getTags());
+                model.setPerson(person, editedPerson);
+            }
+        }
         model.setGroup(oldGroup, newGroup);
+        model.commitAddressBook();
 
         return new CommandResult(String.format(MESSAGE_SUCCESS, oldGroupName, newGroupName));
     }
