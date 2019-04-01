000fd99f4f5a1fe95c379648591a258236e84993
weizhang05 <chanweizhang05@hotmail.com>
2019-04-01 23:11:25 +0800

    added randomized group allocation algorithm in RandomizeCommand

--- src/main/java/seedu/address/logic/commands/RandomizeCommand.java
+++ src/main/java/seedu/address/logic/commands/RandomizeCommand.java
@@ -1,11 +1,14 @@
 package seedu.address.logic.commands;
 
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.parser.EditCommandParser;
+import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.Model;
 import seedu.address.model.grouping.Group;
 import seedu.address.model.participant.Person;
@@ -18,14 +21,16 @@ public class RandomizeCommand extends Command {
 
     public static final String COMMAND_WORD = "randomize";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Evenly distribute all participants across all groups.";
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Evenly distribute all participants across all groups.";
 
     public static final String MESSAGE_SUCCESS = "Participants are evenly distributed";
-    public static final String MESSAGE_FAILURE = "Unable to distribute participants";
+    public static final String MESSAGE_FAILURE = "Unable to distribute participants "
+            + "(min 2 groups and 2 participants)";
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) throws CommandException {
-        List<Person> participants = model.getFilteredPersonList();
+        List<Person> participants = new ArrayList<>(model.getFilteredPersonList());
         List<Group> groups = model.getFilteredGroupList();
 
         /**
@@ -37,8 +42,25 @@ public class RandomizeCommand extends Command {
                     + "\nNum groups: " + groups.size());
         }
 
-        Collections.shuffle(groups, new SecureRandom());
+        List<Integer> jumbledOrder = new ArrayList<>();
+        for (int i = 1; i <= participants.size(); i++) {
+            jumbledOrder.add(i);
+        }
+        Collections.shuffle(jumbledOrder, new SecureRandom());
 
+        Command command;
+        int counter = 0;
+
+        for (int i = 0; i < participants.size(); i++) {
+            try {
+                command = new EditCommandParser().parse(jumbledOrder.get(i)
+                        + " g/" + groups.get(counter % groups.size()).getGroupName());
+                command.execute(model, history);
+                ++counter;
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
 
         return new CommandResult(MESSAGE_SUCCESS);
     }
