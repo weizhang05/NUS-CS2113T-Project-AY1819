6e2eacd232d26d815b612efee35459996a69c133
shanseet <e0202323@u.nus.edu>
2019-04-07 20:26:26 +0800

    update AddHouseCommand test

--- src/test/java/seedu/address/logic/commands/AddHouseCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddHouseCommandTest.java
@@ -1,16 +1,24 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import seedu.address.logic.CommandHistory;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.ModelManager;
+import seedu.address.model.grouping.House;
 
 public class AddHouseCommandTest {
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
+    private static final CommandHistory EMPTY_COMMAND_HISTORY = new CommandHistory();
     private CommandHistory commandHistory = new CommandHistory();
 
     @Test
@@ -19,19 +27,27 @@ public class AddHouseCommandTest {
         new AddHouseCommand(null);
     }
 
-    //Updates to model needed
-    /*
     @Test
     public void execute_duplicateHouse_throwsCommandException() throws Exception {
         AddHouseCommand addHouseCommand = new AddHouseCommand("1");
-        HouseStorage houseStorage = new HouseStorage();
         ModelManager modelManager = new ModelManager();
 
-        modelManager.addHouse("1");
+        modelManager.addHouse(new House("1"));
 
         thrown.expect(CommandException.class);
         thrown.expectMessage(AddHouseCommand.MESSAGE_DUPLICATE_HOUSE);
         addHouseCommand.execute(modelManager, commandHistory);
     }
-    */
+
+    @Test
+    public void execute_addHouseSuccessful() throws CommandException {
+        ModelManager modelManager = new ModelManager();
+        House validHouse = new House("Red");
+
+        CommandResult commandResult = new AddHouseCommand("Red").execute(modelManager, commandHistory);
+
+        assertEquals(String.format(AddHouseCommand.MESSAGE_SUCCESS, "Red"), commandResult.getFeedbackToUser());
+        assertEquals(validHouse, modelManager.getFilteredHouseList().get(0));
+        assertEquals(EMPTY_COMMAND_HISTORY, commandHistory);
+    }
 }
