f369dc1946eac50dc1a94ebc2dcd3160c56c3fd8
shanseet <e0202323@u.nus.edu>
2019-04-07 20:38:03 +0800

    update AddGroupCommand test

--- src/test/java/seedu/address/logic/commands/AddGroupCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddGroupCommandTest.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -7,7 +9,8 @@ import org.junit.rules.ExpectedException;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.ModelManager;
-import seedu.address.storage.HouseStorage;
+import seedu.address.model.grouping.Group;
+import seedu.address.model.grouping.House;
 
 public class AddGroupCommandTest {
 
@@ -21,12 +24,18 @@ public class AddGroupCommandTest {
     @Test
     public void constructor_nullGroup_throwsNullPointerException() {
         thrown.expect(NullPointerException.class);
-        new AddGroupCommand(null, null);
+        new AddGroupCommand(null, "Red");
     }
+
+    @Test
+    public void constructor_nullHouse_throwsNullPointerException() {
+        thrown.expect(NullPointerException.class);
+        new AddGroupCommand("G1", null);
+    }
+
     @Test
     public void execute_nonexistentHouse_throwsCommandException() throws Exception {
         AddGroupCommand addGroupCommand = new AddGroupCommand("1", "2");
-        HouseStorage houseStorage = new HouseStorage();
         ModelManager modelManager = new ModelManager();
 
         thrown.expect(CommandException.class);
@@ -34,17 +43,32 @@ public class AddGroupCommandTest {
         addGroupCommand.execute(modelManager, commandHistory);
     }
 
-    //Updates to model needed
-    /*
     @Test
     public void execute_duplicateGroup_throwsCommandException() throws Exception {
-        AddGroupCommand addGroupCommand = new AddGroupCommand("1", "2");
-        HouseStorage houseStorage = new HouseStorage();
+        AddGroupCommand addGroupCommand = new AddGroupCommand("G1", "Green");
         ModelManager modelManager = new ModelManager();
 
+        modelManager.addHouse(new House("Green"));
+        modelManager.addGroup(new Group("G1", "Green"));
+
         thrown.expect(CommandException.class);
-        thrown.expectMessage(AddGroupCommand.MESSAGE_NONEXISTENT_HOUSE);
+        thrown.expectMessage(AddGroupCommand.MESSAGE_DUPLICATE_GROUP);
         addGroupCommand.execute(modelManager, commandHistory);
     }
-    */
+
+    @Test
+    public void execute_addGroupSuccessful() throws CommandException {
+        ModelManager modelManager = new ModelManager();
+        House validHouse = new House("Red");
+        Group validGroup = new Group("R1", "Red");
+
+        modelManager.addHouse(validHouse);
+
+        CommandResult commandResult =
+                new AddGroupCommand("R1", "Red").execute(modelManager, commandHistory);
+
+        assertEquals(String.format(AddGroupCommand.MESSAGE_SUCCESS, "R1"), commandResult.getFeedbackToUser());
+        assertEquals(validGroup, modelManager.getFilteredGroupList().get(0));
+        assertEquals(EMPTY_COMMAND_HISTORY, commandHistory);
+    }
 }
