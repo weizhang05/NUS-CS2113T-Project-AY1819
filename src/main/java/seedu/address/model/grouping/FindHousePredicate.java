7597424a88cb26a25511485a16ac858e3014929a
shanseet <e0202323@u.nus.edu>
2019-04-09 16:25:59 +0800

    add list by house command

--- src/main/java/seedu/address/model/grouping/FindHousePredicate.java
+++ src/main/java/seedu/address/model/grouping/FindHousePredicate.java
@@ -1,4 +1,30 @@
 package seedu.address.model.grouping;
 
-public class FindHousePredicate {
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.model.participant.Person;
+
+public class FindHousePredicate implements Predicate<Person> {
+
+    private final List<String> keywords;
+
+    public FindHousePredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return keywords.stream()
+                .allMatch(keyword-> StringUtil.containsWordIgnoreCase(person.getGroup().getHouseName(), keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof FindHousePredicate // instanceof handles nulls
+                && keywords.equals(((FindHousePredicate) other).keywords)); // state check
+    }
 }
+
