d97723f5fa001061327079d90ed0dd1623d2fb71
ssunil3232 <soniasunil47@gmail.com>
2019-04-09 16:53:37 +0800

    checkstyle error

--- src/main/java/seedu/address/logic/commands/ExportFreshmenCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportFreshmenCommand.java
@@ -53,4 +53,5 @@ public class ExportFreshmenCommand extends Command {
             return false;
         }
     }
+
 }
--- src/main/java/seedu/address/logic/commands/ExportOglCommand.java
+++ src/main/java/seedu/address/logic/commands/ExportOglCommand.java
@@ -53,4 +53,5 @@ public class ExportOglCommand extends Command {
             return false;
         }
     }
+
 }
